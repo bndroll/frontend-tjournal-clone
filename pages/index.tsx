03f0f7f85398a09c2349cbfa50d609839dcd2059
bounderoll <bounderoll.23@gmail.com>
2021-09-13 23:49:21 +0400

    init: start tjournal-clone and add libraries

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Paper from "@material-ui/core/Paper"
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>TJournal Clone</title>
+            </Head>
+            <Paper>
+                qwe
+            </Paper>
+        </div>
+    )
+}
